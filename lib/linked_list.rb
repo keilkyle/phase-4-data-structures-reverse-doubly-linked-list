98df6e1e45c6efb26bbe95831d0de889517ea63a
keilkyle <98378148+keilkyle@users.noreply.github.com>
2022-07-22 22:08:52 -0700

    commit tree 7610ca27cb3c7d283940fc07885e4222c6d095e3

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,30 @@ class LinkedList
   end
 
   def reverse!
+    if head == nil
+      return
+    elsif head.next_node == nil
+      return
+    end
+
+    current_node = head
+
+    while current_node
+      before = current_node.prev_node
+      after = current_node.next_node
+      current_node.prev_node = after
+      current_node.next_node = before
+
+      current_node = after
+    end
+
+    headOrig = head
+    tailOrig = tail
+    
+    @head = tailOrig
+    @tail = headOrig
+
+
     # your code here
   end
 end
